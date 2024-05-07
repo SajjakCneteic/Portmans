4b316d42f144f9b870b3579afbd62f5e7bdc58bb
VishalGCnetric <vishal@cnetric.com>
2024-05-07 17:34:21 +0530

    redux_toolkit

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import LogIn from './LogIn'
 import SignUp from './SignUp'
 import ShopPage from './ShopPage'
+import HomePage from './HomePage'
 
 const AllRoutes = () => {
   return (
     <Routes>
-        <Routes path="/" element={<HomePage />} />
-        <Routes path="/login" element={<LogIn />} />
-        <Routes path="/signup" element={<SignUp />} />
-        <Routes path="/shop" element={<ShopPage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/shop" element={<ShopPage/>} />
 
     </Routes>
   )
