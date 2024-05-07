77347d8289a6689243a5d82502fd85900114c906
SajjakCnetric <sajjak@cnetric.com>
2024-05-07 17:36:40 +0530

    done

--- src/components/NavigationDropdown.jsx
+++ src/components/NavigationDropdown.jsx
@@ -101,7 +101,7 @@ const NavigationDropdown = () => {
                 {data.extraData?.map((el, index) => <div
                     onMouseEnter={() => document.getElementById(`${el.name}`).style.display = 'block'}
                     onMouseLeave={() => document.getElementById(`${el.name}`).style.display = 'none'}
-                    key={index}><a href="" style={{textTransform:'uppercase',textDecoration:'none',color:'#333',color:`${el.name=='SALE'?'red':'black'}`}}>{el.name}</a>
+                    key={index}><a href="" style={{textTransform:'uppercase',textDecoration:'none',color:`${el.name=='SALE'?'red':'#333'}`}}>{el.name}</a>
                     <DropDownHover childrenData={el} />
                 </div>)}
             </Container>
@@ -113,8 +113,6 @@ export default NavigationDropdown;
 
 
 const DropDownHover = ({ childrenData }) => {
-    console.log(childrenData?.children)
-    console.log(childrenData?.image?.[0])
 
     return (
         <DropDown
